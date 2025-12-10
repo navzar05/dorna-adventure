f09e2f8791eb32e78566f947f67f131765aa20cc
Leanca Razvan <andrei.leanca@mta.ro>
2025-12-10 19:21:53 +0200

    cors for deploy fe

--- backend/src/main/java/ro/atm/backend/config/CorsConfig.java
+++ backend/src/main/java/ro/atm/backend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5174", "http://192.168.1.135:5174", "http://188.25.208.32:6969"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5174", "https://frontend-production-cca2.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
